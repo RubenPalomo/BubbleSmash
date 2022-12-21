f441aefe1848ecd2fec1cabbb7532410538a0e15
Rubén Palomo Fontán <99551384+RubenPalomo@users.noreply.github.com>
2022-12-21 11:16:31 +0100

    Add files via upload

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { useState } from "react";
+import Bubble from "../../components/Bubble/Bubble";
+import "./Home.scss";
+
+function Home() {
+  const [game, setGame] = useState(null);
+  const [score, setScore] = useState(0);
+  const [menuVisible, setMenuVisible] = useState(true);
+  const [isBubbleVisible, setIsBubbleVisible] = useState(false);
+  const [top, setTop] = useState(Math.random() * window.innerHeight);
+  const [left, setLeft] = useState(Math.random() * window.innerWidth);
+  const start = () => {
+    setTop(Math.random() * window.innerHeight);
+    setLeft(Math.random() * window.innerHeight);
+    setIsBubbleVisible(true);
+  };
+  const pause = () => {
+    setMenuVisible(true);
+    setIsBubbleVisible(false);
+    clearInterval(game);
+  };
+  const endGame = () => {
+    setScore(0);
+    setGame(null);
+  };
+  const win = () => {
+    pause();
+    endGame();
+    alert("You win!");
+  };
+  const bubbleSmash = () => {
+    setScore(score + 1);
+    setIsBubbleVisible(false);
+    if (score === 10) {
+      clearInterval(game);
+      setGame(window.setInterval(start, 500));
+    } else if (score === 25) {
+      clearInterval(game);
+      setGame(window.setInterval(start, 300));
+    } else if (score === 45) {
+      clearInterval(game);
+      setGame(window.setInterval(start, 100));
+    } else if (score === 50) win();
+  };
+  const handleClick = () => {
+    setIsBubbleVisible(true);
+    setMenuVisible(false);
+    setGame(window.setInterval(start, 1000));
+  };
+
+  return (
+    <div className="game-container">
+      <div className="counter" onClick={pause}>
+        {score}
+      </div>
+      <div className="menu" hidden={!menuVisible}>
+        <h1>{game === null ? "Menu" : "Pause"}</h1>
+        <div className="buttons">
+          <button onClick={handleClick}>
+            {game === null ? "Start!" : "Continue"}
+          </button>
+          <br />
+          <button hidden={game === null ? true : false} onClick={endGame}>
+            Exit
+          </button>
+        </div>
+      </div>
+      <div
+        onMouseEnter={bubbleSmash}
+        onClick={bubbleSmash}
+        hidden={!isBubbleVisible}
+      >
+        <Bubble top={top} left={left} />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
